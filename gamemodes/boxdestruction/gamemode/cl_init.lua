b85bed143806b7bd279fbee9484723ce725151f3
MichaloxBruh <mf.etutor@outlook.com>
2022-10-30 19:43:08 +0100

    Basic functions added

--- gamemodes/boxdestruction/gamemode/cl_init.lua
+++ gamemodes/boxdestruction/gamemode/cl_init.lua
@@ -1,15 +1,3 @@
 include("shared.lua")
 
-function GM:PlayerConnect(name, ip)
-
-	print("Player joined with ("..ip..")")
-end
-
-
-
-function GM:PlayerInitialSpawn(ply)
-
-	Print("Hello, " .. ply:Name() .. " has spawned!")
-end 
-
 
--- gamemodes/boxdestruction/gamemode/init.lua
+++ gamemodes/boxdestruction/gamemode/init.lua
@@ -2,3 +2,15 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include("shared.lua")
+
+function GM:PlayerInitialSpawn(ply)
+    ply:SetModel("models/player/combine_super_soldier.mdl")
+    ply:SetHealth("200")
+    ply:SetMaxHealth("200")
+    ply:Give("weapon_frag", false)
+end
+
+function GM:PlayerSpawn(ply)
+    ply:SetHealth("200")
+    ply:Give("weapon_frag", false)
+end
